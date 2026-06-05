0e117ae912e4349e87cc7a5bb55463b92db205e2
Matias <maatias.echeverria@gmail.com>
2026-06-04 23:39:33 -0300

    Layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const FavoritosPage = () => import('../modules/favoritos/page/FavoritosPage.vue')
+const DetalleJuegoPage = () => import('../modules/detalle-juego/page/DetalleJuegoPage.vue')
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/favoritos',
+    },
+    {
+      path: '/favoritos',
+      name: 'favoritos',
+      component: FavoritosPage,
+    },
+    {
+      path: '/juegos/:id',
+      name: 'detalle-juego',
+      component: DetalleJuegoPage,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/favoritos',
+    },
+  ],
 })
 
 export default router
